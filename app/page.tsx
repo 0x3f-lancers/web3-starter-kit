b0c244c6c3596c77b11bfe7b95b666a9994ea3c5
Ad-h0c <reddycsivaprakash5@gmail.com>
2023-11-19 17:22:15 +0530

    rainbow-nextjs template

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center text-center justify-center p-24 space-y-4 ">
+      <h1 className="text-3xl font-extrabold">Nextjs-rainbow kit template</h1>
+      <h2 className="text-2xl font-bold">clone the repo</h2>
+      <h3 className="text-xl font-semibold">Install dependencies</h3>
+      <h4 className="text-lg font-medium">Start working</h4>
+    </main>
+  );
+}
